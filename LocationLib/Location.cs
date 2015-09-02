f8069d7e99c1a3a621372fc0851fab5b998f79ed
Lars Thorup <lars@zealake.com>
2015-09-02 14:51:01 +0200

    Location.ToDisplayString

---
+++ LocationLib/Location.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocationLib
+{
+    public class Location
+    {
+        public string CountryCode { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string City { get; set; }
+
+        public string ToDisplayString()
+        {
+            if (CountryCode == "US")
+            {
+                return string.Format("{0}, {1}", City, Region);
+            }
+            else
+            {
+                if (String.IsNullOrEmpty(City))
+                {
+                    return Country;
+                }
+                else
+                {
+                    return string.Format("{0}, {1}", City, Country);
+                }
+            }
+        }
+
+    }
+}
---
+++ LocationLibTest/LocationTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LocationLib;
+
+namespace LocationLibTest
+{
+    [TestClass]
+    public class LocationTest
+    {
+        [TestMethod]
+        public void ToDisplayString_InsideUsa()
+        {
+            var location = new Location { CountryCode = "US", Region = "California", City = "San Francisco" };
+            Assert.AreEqual("San Francisco, California", location.ToDisplayString());
+        }
+
+        [TestMethod]
+        public void ToDisplayString_OutsideUsa()
+        {
+            var location = new Location { CountryCode = "DK", Country = "Denmark", Region = "Hovedstaden", City = "Farum" };
+            Assert.AreEqual("Farum, Denmark", location.ToDisplayString());
+        }
+
+        [TestMethod]
+        public void ToDisplayString_NoCity()
+        {
+            var location = new Location { CountryCode = "PH", Country = "Philippines", Region = "", City = "" };
+            Assert.AreEqual("Philippines", location.ToDisplayString());
+        }
+    }
+}
