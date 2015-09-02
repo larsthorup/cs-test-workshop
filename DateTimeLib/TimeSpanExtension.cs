3a88b1ac64b81a3fdaa67c393a32b91d4a8ba2d1
Lars Thorup <lars@zealake.com>
2015-09-02 16:19:08 +0200

    TimeSpan.ToDisplayString

---
+++ DateTimeLib/TimeSpanExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeLib
+{
+    public static class TimeSpanExtension
+    {
+        public static string ToDisplayString(this TimeSpan timeSpan)
+        {
+            if (timeSpan < TimeSpan.FromSeconds(5))
+            {
+                return "now";
+            }
+            else if (timeSpan < TimeSpan.FromMinutes(1))
+            {
+                return string.Format("{0} seconds ago", timeSpan.Seconds.ToString());
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ DateTimeLibTestMs/TimeSpanTestMs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DateTimeLib;
+
+namespace DateTimeLibTestMs
+{
+    [TestClass]
+    public class TimeSpanTestMs
+    {
+        [TestMethod]
+        public void ToDisplayString_Now()
+        {
+            Assert.AreEqual("now", TimeSpan.FromSeconds(4).ToDisplayString());
+        }
+
+        [TestMethod]
+        public void ToDisplayString_Seconds()
+        {
+            Assert.AreEqual("5 seconds ago", TimeSpan.FromSeconds(5).ToDisplayString());
+        }
+    }
+}
