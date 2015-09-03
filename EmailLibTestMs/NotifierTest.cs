4aebd481668cc8ee6808eefd0f3947470838af92
Lars Thorup <lars@zealake.com>
2015-09-03 11:40:53 +0200

    Notifier.Trigger

--- EmailLibTestMs/NotifierTest.cs
+++ EmailLibTestMs/NotifierTest.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EmailLib;
 using NSubstitute;
 
-namespace EmailLibTestMsRhino
+namespace EmailLibTestMs
 {
     [TestClass]
     public class NotifierTest
